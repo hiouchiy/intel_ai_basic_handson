0b830f15722c64cdcadfb6efa9c03f271add4d59
houchiya <hiroshi.ouchiyama@intel.com>
2020-10-21 21:30:03 +0900

    Upload

---
+++ ml/kmeans.py
@@ -0,0 +1,30 @@
+from sklearn.datasets.samples_generator import make_blobs
+import numpy as np
+import time
+
+#start = time.time()
+#x, y = make_blobs(n_samples = 2 * 10**6, n_features = 50,
+#                  centers = 10, cluster_std = 0.2,
+#                  center_box = (-10.0, 10.0), random_state = 777)
+#np.savetxt("kmeans_data.csv", x, fmt = "%f", delimiter = ",")
+#print('{} sec'.format(time.time()-start))
+
+
+import numpy as np
+import pandas as pd
+import sklearn as skl
+from sklearn.cluster import KMeans
+# Set config to prevent long finiteness check of input data
+skl.set_config(assume_finite = True)
+# Fast CSV reading with pandas
+start = time.time()
+train_data = pd.read_csv("kmeans_data.csv", dtype = np.float32)
+print('{} sec'.format(time.time()-start))
+
+alg = KMeans(n_clusters = 10, init = "random", tol = 0,
+                         algorithm = "full", max_iter = 50)
+start = time.time()
+alg.fit(train_data)
+print('{} sec'.format(time.time()-start))
+
+
---
+++ ml/svm.py
@@ -0,0 +1,89 @@
+import numpy as np
+def convert_data(data, dtype, data_order, data_format):
+    '''
+    Convert input data (numpy array) to needed format, type and order
+    '''
+    # Firstly, change order and type of data
+    if data_order == 'F':
+        data = np.asfortranarray(data, dtype)
+    elif data_order == 'C':
+        data = np.ascontiguousarray(data, dtype)
+
+    # Secondly, change format of data
+    if data_format == 'numpy':
+        return data
+    elif data_format == 'pandas':
+        import pandas as pd
+
+        if data.ndim == 1:
+            return pd.Series(data)
+        else:
+            return pd.DataFrame(data)
+    elif data_format == 'cudf':
+        import cudf
+        import pandas as pd
+
+        return cudf.DataFrame.from_pandas(pd.DataFrame(data))
+
+def load_data(shape=(1000000, 50), generated_data=[], add_dtype=False, label_2d=False,
+              int_label=False):
+    full_data = {
+        file: None for file in ['X_train', 'X_test', 'y_train', 'y_test']
+    }
+    #param_vars = vars(params)
+    #print(full_data)
+    int_dtype = np.int32 if '32' in 'numpy.float64' else np.int64
+    for element in full_data:
+        # generate and convert data if it's marked and path isn't specified
+        if full_data[element] is None and element in generated_data:
+            full_data[element] = convert_data(
+                np.random.rand(*shape),
+                int_dtype if 'y' in element and int_label else np.float64,
+                'C', 'pandas')
+        # convert existing labels from 1- to 2-dimensional
+        # if it's forced and possible
+        if full_data[element] is not None and 'y' in element and label_2d and hasattr(full_data[element], 'reshape'):
+            full_data[element] = full_data[element].reshape(
+                (full_data[element].shape[0], 1))
+        # add dtype property to data if it's needed and doesn't exist
+        if full_data[element] is not None and add_dtype and not hasattr(full_data[element], 'dtype'):
+            if hasattr(full_data[element], 'values'):
+                full_data[element].dtype = full_data[element].values.dtype
+            elif hasattr(full_data[element], 'dtypes'):
+                full_data[element].dtype = full_data[element].dtypes[0].type
+
+    # clone train data to test if test data is None
+    for data in ['X', 'y']:
+        if full_data[f'{data}_train'] is not None and full_data[f'{data}_test'] is None:
+            full_data[f'{data}_test'] = full_data[f'{data}_train']
+    return tuple(full_data.values())
+
+
+import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from numpy.random import *
+from sklearn.svm import SVC
+import time
+
+# generate regression dataset
+print('Start datagen')
+start = time.time()
+X_train, X_test, y_train, y_test = load_data(shape=(10000, 10), generated_data=['X_train', 'y_train'])
+y_train = randint(0,100,10000)
+print('{} sec'.format(time.time()-start))
+print('End datagen')
+
+print()
+print('Start train')
+clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+clf.fit(X_train, y_train)
+print('{} sec'.format(time.time()-start))
+print('End train')
+
+print()
+print('Start prediction')
+clf.predict(X_test)
+print('{} sec'.format(time.time()-start))
+print('End prediction')
+
